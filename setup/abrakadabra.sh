1fcb114124d31d41814f553d9479062a216650c8
Lukasz Rabiec <lukaszrabiec@gmail.com>
2026-03-03 14:13:25 +0100

    feat: use kitty as temporary term for tmux and yazi

--- setup/abrakadabra.sh
+++ setup/abrakadabra.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-. ./remove-webapps.sh
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-. ./install-hyprsession.sh
-. ./install-lsd.sh
-. ./install-7zip.sh
-. ./install-yazi.sh
-. ./install-rclone.sh
-. ./install-diff-so-fancy.sh
-. ./install-tmux-plugin-manager.sh
+. "$SCRIPT_DIR/remove-webapps.sh"
+
+. "$SCRIPT_DIR/install-hyprsession.sh"
+. "$SCRIPT_DIR/install-lsd.sh"
+. "$SCRIPT_DIR/install-7zip.sh"
+. "$SCRIPT_DIR/install-yazi.sh"
+. "$SCRIPT_DIR/install-rclone.sh"
+. "$SCRIPT_DIR/install-diff-so-fancy.sh"
+. "$SCRIPT_DIR/install-tmux-plugin-manager.sh"
+
+# TMP
+. "$SCRIPT_DIR/install-kitty.sh"
---
+++ setup/install-kitty.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+omarchy-install-terminal kitty
+
+# Using kitty for now because of ghostty issue with tmux and yazi
+# https://github.com/ghostty-org/ghostty/discussions/4266
+# When the issue is resolved switch back to ghostty
