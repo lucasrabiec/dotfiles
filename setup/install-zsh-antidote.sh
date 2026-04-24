c8df819f6c40724bb00c79cc54f514b612c0265c
Lukasz Rabiec <lukaszrabiec@gmail.com>
2026-04-24 09:29:15 +0200

    feat: remove vim for zsh and set blinking cursor

--- setup/install-zsh-antidote.sh
+++ setup/install-zsh-antidote.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/mattmc3/antidote.git "${ZDOTDIR:-$HOME}"/.antidote
+target_dir="${ZDOTDIR:-$HOME}/.antidote"
+
+if [ ! -d "$target_dir" ]; then
+  echo "Cloning antidote..."
+  git clone --depth=1 https://github.com/mattmc3/antidote.git "$target_dir"
+else
+  echo "Repository already exists at $target_dir — skipping clone"
+fi
